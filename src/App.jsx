210b69c021e52fed828c7fe31c3f23bd1c5ce1cf
TadhgSyme <s4096052@student.rmit.edu.au>
2026-03-20 20:14:15 +1100

    Create src/App.jsx with Book Basket React component

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const BookBasket = () => {
+    return (
+        <div>
+            <h1>Book Basket</h1>
+            {/* Your Book Basket component code goes here */}
+        </div>
+    );
+};
+
+export default BookBasket;
